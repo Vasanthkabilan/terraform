a4e1e740769e0162e1b116b1b66050ed726dd173
vasanthkabilan <vasanthkabilan91@gmail.com>
2022-02-22 11:44:51 +0530

    variables added for ELB

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,3 +1,19 @@
 variable "vpn_ip" {
     default = "10.30.45.23/32"
 }
+
+variable "usernumber" {
+  type = number
+}
+
+variable "elb_name" {
+  default = "myelb"
+}
+
+variable "regions" {
+  type = list(string)
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+variable "timeout" {
+  default = 400
+}
